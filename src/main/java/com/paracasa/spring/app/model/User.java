43bf54a9e2eefa11f84249c44fc2439daf6a3b29
alu10213035 <martaortega2411@gmail.com>
2023-02-06 15:52:37 +0100

    user

--- src/main/java/com/paracasa/spring/app/model/User.java
+++ src/main/java/com/paracasa/spring/app/model/User.java
@@ -1,31 +1,39 @@
 package com.paracasa.spring.app.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "User")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String nombre;
-    private String contrasenya;
+    private String name;
+    private String password;
 
-    public String getContrasenya() {
-        return contrasenya;
+    public User(String name) {
+        this.setName(name);
+        this.setId((long) (Math.random() * (1000 - 1) + 1));
     }
 
-    public String getNombre() {
-        return nombre;
+    public User() {
+        this.setId((long) (Math.random() * (1000 - 1) + 1));
     }
 
-    public void setContrasenya(String contrasenya) {
-        this.contrasenya = contrasenya;
+    public String getPassword() {
+        return password;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String contrasenya) {
+        this.password = contrasenya;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setId(Long id) {
